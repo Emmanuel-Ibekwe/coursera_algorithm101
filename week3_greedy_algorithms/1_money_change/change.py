325e50be6da02eac46040f57870d01b499ac6d52
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-03-14 14:21:19 +0100

    Did the first two assignments in week 3

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -1,7 +1,17 @@
 def change(money):
     # write your code here
-
-    return money
+    num_coins = 0
+    while money > 0:
+        if money >= 10:
+            num_coins = num_coins + money // 10
+            money = money % 10
+        elif money >= 5:
+            num_coins = num_coins + money // 5
+            money = money % 5
+        else:
+            num_coins = num_coins + money
+            money = 0
+    return num_coins
 
 
 if __name__ == '__main__':
--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -1,11 +1,42 @@
 from sys import stdin
 
+def insertion_sort_desc(arr, weights, values):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        w_key = weights[i]
+        v_key = values[i]
+        j = i - 1
+        while j >= 0 and arr[j] < key:
+            arr[j + 1] = arr[j]
+            weights[j + 1] = weights[j]
+            values[j + 1] = values[j]
+            j -= 1
+        arr[j + 1] = key
+        weights[j + 1] = w_key
+        values[j + 1] = v_key
+
+def sorted_unit_values(values, weights):
+    unit_values = []
+    for i in range(len(values)):
+        unit_value = values[i]/weights[i]
+        unit_values.append(unit_value)
+    insertion_sort_desc(unit_values, weights, values)
+
+        
 
 def optimal_value(capacity, weights, values):
-    value = 0.
-    # write your code here
-
-    return value
+    sorted_unit_values(values, weights)
+    # value = 0.
+    total_value = 0
+    for i in range(len(values)):
+        if capacity == 0:
+            return total_value
+        amt = min(weights[i], capacity)
+        total_value = total_value + amt * (values[i]/weights[i])
+        weights[i] = weights[i] - amt
+        capacity -= amt
+    
+    return total_value
 
 
 if __name__ == "__main__":
@@ -15,3 +46,6 @@ if __name__ == "__main__":
     weights = data[3:(2 * n + 2):2]
     opt_value = optimal_value(capacity, weights, values)
     print("{:.10f}".format(opt_value))
+
+
+
