fefc5efa70a7389495448430bdfa41999c71f108
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-03-15 12:46:04 +0100

    Did the third assignment in week 3

--- week3_greedy_algorithms/3_car_fueling/car_fueling.py
+++ week3_greedy_algorithms/3_car_fueling/car_fueling.py
@@ -3,7 +3,35 @@ from sys import stdin
 
 def min_refills(distance, tank, stops):
     # write your code here
-    return -1
+    current_stop = 0
+    next_stop = 0
+    remaining_mileage = tank
+    total_distance = 0
+    refills = 0
+    n = len(stops)
+    for i in range(n):
+        current_stop = stops[i]
+        if i < n - 1:
+            next_stop = stops[i + 1]
+        else: 
+            next_stop = distance
+        if i == 0: 
+            remaining_mileage = remaining_mileage - stops[i]
+            total_distance = stops[i]
+            
+        if total_distance + remaining_mileage < next_stop:
+            refills += 1
+            remaining_mileage = tank
+        if total_distance + remaining_mileage < next_stop:
+            return -1
+  
+        total_distance = next_stop
+        remaining_mileage = (current_stop + remaining_mileage) - total_distance
+
+    return refills
+        
+
+        
 
 
 if __name__ == '__main__':
